35074cdb1c04f4a0e2840e0610fd7c22a170faaf
ford-narongrit <narongrit.th@ku.th>
2021-07-13 14:45:48 +0700

    add NumberList

---
+++ src/components/Counter.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="mb-20">
+    Counter: {{ number }}
+    <div>
+      <label for="counter">Counter: </label>
+      <input type="text" v-model="number" />
+      <button v-on:click="increase">plus 1</button>
+      <button v-on:click="decrease">miuns 1</button>
+      <button v-on:click="increaseTen">plus 10</button>
+      <button v-on:click="timeTwo">X2</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 10,
+    };
+  },
+  methods: {
+    increase() {
+      this.number++;
+    },
+    decrease() {
+      this.number--;
+    },
+    increaseTen() {
+        this.number+=10;
+    },
+    timeTwo() {
+        this.number*=2;
+    }
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.mb-20 {
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/views/NumberList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <counter></counter>
+      <counter></counter>
+      <counter></counter>
+      <counter></counter>
+      <counter></counter>
+      <counter></counter>
+  </div>
+</template>
+
+<script>
+import Counter from '../components/Counter.vue'
+export default {
+    components: {
+        Counter
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
